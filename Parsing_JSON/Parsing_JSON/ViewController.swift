c53dbab42e752d8b8b28d7b54affab01b777df64
Ashwini shalke <ashwinishalke050@gmail.com>
2019-08-30 23:49:36 +0530

    Implements JSON parsing using JSONDecoder

--- Parsing_JSON/Parsing_JSON/ViewController.swift
+++ Parsing_JSON/Parsing_JSON/ViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-struct courses {
-    let id : Int
+
+struct WebsiteDescription: Decodable{
     let name : String
-    let link : String
-    let imageURL  : String
-    let numberOfLesson : Int
+    let description : String
+    let courses : [Courses]
+}
+
+struct Courses: Decodable{
+    let id : Int?
+    let name : String?
+    let link : String?
+    let imageURL  : String?
+    let numberOfLesson : Int?
+    
+    //    init(json: [String: Any]){
+    //
+    //        id = json["id"] as? Int ?? -1
+    //        name = json["name"] as? String ?? " "
+    //        link = json["link"] as? String ?? " "
+    //        imageURL = json["imageURL"] as?  String ?? " "
+    //        numberOfLesson = json["numberOfLesson"] as? Int ?? -1
+    //    }
 }
 
 class ViewController: UIViewController {
@@ -21,28 +37,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
         guard let url = URL(string: jsonURLString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, err) in
-            guard let data = data else {return}
+            
+            guard let data = data else { return }
             
             // converting JSON data into String
-            //            let dataAsString = String(data: data, encoding: .utf8 )
+            // let dataAsString = String(data: data, encoding: .utf8 )
             // print (dataAsString)
-
-            // Swift 2/3 uses JSONSeriliazation
+            
             do {
-                let json = try JSONSerialization.jsonObject(with: data, options:.mutableContainers)
-                print(json)
+                // Swift 2/3 uses JSONSeriliazation
+                // guard let json = try JSONSerialization.jsonObject(with: data, options:.mutableContainers)
+                // as? [String: Any] else { return}
+                let course = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+                print(course.courses)
+                
             } catch let jsonErr {
                 print("Error in JSON Serilization", jsonErr)
             }
-            
-            
-            
             }.resume()
     }
-    
     
 }
 
